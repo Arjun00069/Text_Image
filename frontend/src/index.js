21772b245020389aca641ec45441867ecf9d778e
Arjun00069 <9525876040golukumar@gmail.com>
2024-12-01 16:50:26 +0530

    Latest

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-const server= "https://text-image1.onrender.com//api/v1"
+const server= "https://text-image1.onrender.com/api/v1"
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
